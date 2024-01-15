097acba073c8376512fdcdd3894503cc7a5b8eaf
Logan Martinez <logan_amp@outlook.com>
2024-01-15 15:05:45 -0600

    Gravitational pull implementation

---
+++ world.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+@onready var children = get_tree().current_scene.get_children()
+@onready var player = $Player
+
+var planet_positions = []
+var planets = []
+var direction_vectors = []
+
+
+func _ready():
+	get_planets_positions()
+
+
+func _physics_process(delta):
+	move_player(delta)
+
+
+func get_planets_positions():
+	planets = children.duplicate()
+	planets.pop_at(children.find(player))
+	for planet in planets:
+		planet_positions.append(planet.global_position)
+
+
+func get_distance_player_to_planet():
+	direction_vectors = []
+	var distances = []
+	
+	for planet_position in planet_positions:
+		var direction = planet_position - player.global_position
+		
+		direction_vectors.append(direction.normalized())
+		distances.append((direction).length() / 100)
+	return distances
+
+
+func calculate_gravitational_forces():
+	var planet_distances_to_player = get_distance_player_to_planet()
+	var forces = []
+	
+	for i in range(planets.size()):
+		forces.append(planets[i].GRAVITATIONAL_STRENGTH / (planet_distances_to_player[i] ** 2))
+	return forces
+
+
+func sum_gravitational_vectors():
+	var forces = calculate_gravitational_forces()
+	var resultant_vector = Vector2.ZERO
+	
+	for i in range(forces.size()):
+		resultant_vector += forces[i] * direction_vectors[i]
+	return resultant_vector
+
+
+func move_player(delta):
+	if not player: return
+	player.velocity += sum_gravitational_vectors() * delta
